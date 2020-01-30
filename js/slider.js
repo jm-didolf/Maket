8ce26b6c341036ba78962364c1ea62b15439a814
nikitabeloshapka <existing@live.ru>
2020-01-30 20:03:53 +0500

    111sss

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,33 @@
 window.onload = function() {
-    let hiddenElements = document.querySelectorAll('.brand-block__brands');
-    console.log(hiddenElements);
 
-   function checker(hiddenElements) {
-    for (let i = 0; i < hiddenElements.length; i++){
 
-        hiddenElements[i].classList.add('unvisible');
+
+   function visibleMore () {
+       let hiddenElements = document.querySelectorAll('.brand-block__brands');
+
+       for (let i = 0; i < hiddenElements.length; i++){
+            if (hiddenElements[i].classList.contains("visible")) {
+                hiddenElements[i].classList.remove('visible');
+                hiddenElements[i].classList.add('unvisible');
+                buttonReadMore.innerHTML = "Показать все";
+                iconReadMore.style.transform = "rotate(360deg)";
+            } else {
+                hiddenElements[i].classList.add('visible');
+                buttonReadMore.innerHTML = "Скрыть";
+                iconReadMore.style.transform = "rotate(180deg)";
+                hiddenElements[i].classList.remove('unvisible');
+            }
+
     }
 }
-
+    let iconReadMore = document.querySelector('.icon-read-more');
+    let buttonReadMore = document.querySelector('.read-more');
+        buttonReadMore.addEventListener("click", visibleMore);
 
 
 }
+
+
+
 
 
